bb7f54c7977bb3730fe40169c2f0ed3224f3a860
Walter Goulet <wgoulet@gmail.com>
2014-08-11 03:43:45 +0000

    JSON serialization of data from handsontable in JS now functional; Flask app is able to save/load JSON encoded data from JS

--- hello.py
+++ hello.py
@@ -3,6 +3,8 @@ from flask import request
 from flask import render_template
 from flask import json
 from flask_cors import cross_origin
+from flask import jsonify
+import pickle
 
 
 app = Flask(__name__)
@@ -24,18 +26,31 @@ def submit():
     if request.method == 'POST':
         print "In method"    
         #import pdb; pdb.set_trace()
-        data = request.form
-        for i in data.iteritems(multi=True):
-            print i
-        
-            
+        data = request.json
+        savedata(data)
+        print data    
         #return make_response(data)
-        return "testing"
+        return "Saved"
 
     else:
         print "Unable to parse"
     return "Error"    
 
+@app.route("/timesheet/getdata.json")
+def fetchdata():
+    data = loaddata()
+    return jsonify(data=data)
+     
+
+def loaddata():
+    f = open('datfile','r')
+    return pickle.load(f)
+
+
+def savedata(data):
+    f = open('datfile','w')
+    pickle.dump(data,f)
+    
 
 if __name__ == "__main__":
     app.run("0.0.0.0",5000)
